93265ed1f213a2a7c19ef227ad666a882c1df099
Manuel Hernández <manu.hdez.dev@gmail.com>
2018-08-13 12:32:12 +0100

    finished stage 2 - video 3

--- 1-profile/router.js
+++ 1-profile/router.js
@@ -1,3 +1,5 @@
+const Profile = require('./profile');
+
 const home = (request, response) => {
   if (request.url === '/') {
     response.statusCode = 200;
@@ -13,8 +15,31 @@ const user = (request, response) => {
   if (username.length > 0) {
     response.setHeader('Content-Type', 'text/plain');
     response.write('Header\n');
-    response.write(`${username}\n`);
-    response.end('Footer\n');
+    // Get profile data from treehouse
+    let studentProfile = new Profile(username);
+    // on end
+    studentProfile.on('end', (profileJSON) => {
+      // show profile
+      // Store the values we need
+      let values = {
+        avatarURL: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+      // simple response
+      response.write(`${values.username} has ${values.badges} badges\n`);
+      response.end('Footer\n');
+    });
+
+      // on error
+      studentProfile.on('error', error => {
+        // show error
+        // console.error(error.message);
+        response.write(error.message + "\n");
+        response.end('Footer\n');
+      });
+
   }
 }
 
